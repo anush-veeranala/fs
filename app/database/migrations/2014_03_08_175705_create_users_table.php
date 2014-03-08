199a44519d6dc370cbd12a8b22b9d7c3bf8faa7f
Anush Veeranala <anush.2k10@gmail.com>
2014-03-08 23:55:01 +0530

    user table migration

--- app/database/migrations/2014_03_08_175705_create_users_table.php
+++ app/database/migrations/2014_03_08_175705_create_users_table.php
@@ -5,24 +5,36 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		//
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+        Schema::create('users', function($table)
+        {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('admin')->default(0);
+            $table->timestamps();
+        });
+
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+        //
+    }
 
 }
